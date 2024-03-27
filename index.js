71b080d3dd47bd691f669b3214bdbd7811180a24
audrey cherop <audreycherop06@gmail.com>
2024-03-27 22:32:53 +0300

    added a new file

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+// Function for one to find if the Denver Broncos have won a Superbowl
+function superbowlWin(record) {
+    // Using the find() method to search for a win ("W") in the record
+    const winningYear = record.find(game => game.result === "W");
+  
+    // If a win is found, return the year
+    if (winningYear) {
+      return winningYear.year;
+    } else {
+      // If no win is found, return undefined
+      return undefined;
+    }
+  }
